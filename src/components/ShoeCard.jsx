2d4598eba71189ce87408d0b3f49873f14f23881
dub-ing <duboyeolawale@gmail.com>
2025-08-21 02:21:12 -0700

    added hero section

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,31 @@
+function ShoeCard({ imgUrl, changeBigShoeImage, bigShoeImg }) {
+  console.log(imgUrl);
+
+  function handleClick() {
+    if (bigShoeImg !== imgUrl.bigShoe) {
+      changeBigShoeImage(imgUrl.bigShoe);
+    }
+  }
+  return (
+    <div
+      class={`border-2 rounded-xl ${
+        bigShoeImg === imgUrl.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
+      } cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      <div class="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgUrl.thumbnail}
+          alt="Shoe collection"
+          width={127}
+          height={103}
+          class="object-contain"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ShoeCard;
